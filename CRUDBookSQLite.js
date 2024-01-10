edfc999ab1dadc6ce061cc87b55bca4788873e37
phongpanphokaew <s6206021631029@emailkmutnb.ac.th>
2024-01-10 10:23:10 +0700

    up

--- CRUDBookSQLite.js
+++ CRUDBookSQLite.js
@@ -1,8 +1,11 @@
 const express = require('express'); 
 const sqlite3 = require('sqlite3'); 
 const app = express();
+
 const db = new sqlite3. Database('./Database/Book.sqlite');
+ 
 app.use(express.json());
+
 db.run(`CREATE TABLE IF NOT EXISTS books ( 
     id INTEGER PRIMARY KEY, 
     title TEXT, 
